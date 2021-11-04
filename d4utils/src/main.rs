e610bee4239a36effbf708643ae76995dc900c80
Hao Hou <haohou302@gmail.com>
2021-11-04 07:58:54 -0700

    d4tools create refactor

--- d4utils/src/main.rs
+++ d4utils/src/main.rs
@@ -6,30 +6,32 @@ mod plot;
 mod server;
 mod show;
 mod stat;
+mod utils;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<_> = std::env::args().skip(1).collect();
     let ret = match args.get(0).map(AsRef::as_ref) {
         Some("create") => create::entry_point(args),
         Some("framedump") => framedump::entry_point(args),
-        Some("show") | Some("view") => show::entry_point(args),
-        Some("stat") => stat::entry_point(args),
+        Some("ls-track") => ls_track::entry_point(args),
+        Some("merge") => merge::entry_point(args),
         Some("plot") => plot::entry_point(args),
         Some("serve") => server::entry_point(args),
-        Some("merge") => merge::entry_point(args),
-        Some("ls-track") => ls_track::entry_point(args),
+        Some("show") | Some("view") => show::entry_point(args),
+        Some("stat") => stat::entry_point(args),
         _ => {
             eprintln!("D4 Utilities Program {}", d4::VERSION);
             eprintln!("Usage: d4tools <subcommand> <args>");
             eprintln!("Possible subcommands are:");
             eprintln!("\tcreate   \tCreate a new D4 depth profile");
             eprintln!("\tframedump\tDump The container data");
-            eprintln!("\tview     \tPrint the underlying depth profile");
-            eprintln!("\tstat     \tRun statistics on the given file");
+            eprintln!("\tls-track \tList all available tracks in the D4 file");
+            eprintln!("\tmerge    \tMerge existing D4 file as a multi-track D4 file");
             eprintln!("\tplot     \tPlot the specified region");
             eprintln!("\tserve    \tStart a D4 server");
-            eprintln!("\tmerge    \tMerge existing D4 file as a multi-track D4 file");
-            eprintln!("\tls-track \tList all available tracks in the D4 file");
+            eprintln!("\tshow     \tPrint the underlying depth profile");
+            eprintln!("\tstat     \tRun statistics on the given file");
+            eprintln!("\tview     \tSame as show");
             eprintln!();
             eprintln!("Type 'd4tools <subcommand> --help' to learn more about each subcommands.");
             Ok(())
