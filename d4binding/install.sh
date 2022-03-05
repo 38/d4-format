863f2de225e412610399b4eeffcda98cdda2fac7
Hao Hou <haohou302@gmail.com>
2022-03-05 12:22:53 -0800

    OSX install

--- d4binding/install.sh
+++ d4binding/install.sh
@@ -10,4 +10,4 @@ mkdir -p ${PREFIX}/include/
 mkdir -p ${PREFIX}/lib/
 
 install -m 644 include/d4.h ${PREFIX}/include
-install -m 644 ../target/release/libd4binding.so ${PREFIX}/lib
+install -m 644 ../target/release/libd4binding.* ${PREFIX}/lib
