cd492367717c5ddbb32f9706677b73c27f8e030a
Jakob Willforss <Jakob37@users.noreply.github.com>
2024-10-01 08:48:15 +0200

    Update d4/src/task/context.rs
    Co-authored-by: Chiara Rasi <rasi.chiara@gmail.com>

--- d4/src/task/context.rs
+++ d4/src/task/context.rs
@@ -98,7 +98,7 @@ where
                 }
                 // As ranges are sorted on their left position, the idx approach
                 // above can let through ranges with lower left positions which still
-                // does not reach into the next segment
+                // do not reach into the next segment
                 // See https://github.com/38/d4-format/pull/91
                 if fpl > r {
                     overlapping_idx += 1;
